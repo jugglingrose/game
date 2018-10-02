8023ac72037ce0cae5fb83acde63770d8818e1b9
Corean Gonzales <coreangonzales@gmail.com>
2018-10-02 14:21:05 -0500

    added a class

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 
+/*Images*/
 var blue_club;
 var purple_ball;
 var pink_club;
 var red_ball;
 var fire_torch;
 var unicorn;
+
 var randomX;
 var y;
 var r;
+var ball;
+var img;
+var objectHeight;
+var objectWidth;
 
 /*preload our images*/
 function preload() {
@@ -15,7 +21,6 @@ function preload() {
   pink_club = loadImage('/assets/pink_juggle_club.png');
   red_ball = loadImage('/assets/red_ball.png');
   purple_ball = loadImage('/assets/purple_ball.png');
-
 }
 
 function setup() {
@@ -23,37 +28,56 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
   background(0);
   /*Every few seconds we want a new object to appear at the top of the screen*/
-  setInterval(objectAppear, 3000);
+  setInterval(init, 3000);
 }
 
-/*generate random objects*/
-function objectAppear(){
+/* Item Class */
+function Item(img) {
+  this.img = img;
   randomX = random(1, width-20);
+  y = 15;
+
+  this.update = function(){
+  }
+
+  this.show = function(){
+  }
+
+}
+
+/*generate random img*/
+function imgSelect(){
+  console.log('img select called');
   /*select a random number from 0 to 4*/
   r = Math.floor(Math.random() * 4);
-  /* set y-axis */
-  y = 15;
 
    switch( r ){
     case 0:
-      console.log('purple');
-      image(purple_ball, randomX, y, 25, 25);
-      break;
+      img = "purple_ball";
+      //objectHeight = 25;
+      //objectWidth = 25;
+      return img;
     case 1:
       console.log('blue');
-      image(blue_club, randomX, y, 15, 50);
-      break;
+      img = "blue_club";
+      return img;
     case 2:
       console.log('red');
-      image(red_ball, randomX, y, 25, 25);
-      break;
+      img = "red_ball";
+      return img;
     case 3:
-      image(pink_club, randomX, y, 15, 50);
+      img = "pink_club";
+      return img;
   }
 }
 
+function init() {
+  img = imgSelect();
+  Item(img);
+}
+
 function draw() {
-    y = y + 5;
+  this.show();
   
 }
 
