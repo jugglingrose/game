e369f5888967274d1d9db1bb826e29912d02fb6f
Corean Gonzales <coreangonzales@gmail.com>
2018-10-02 12:25:20 -0500

    generate random objects

--- sketch.js
+++ sketch.js
@@ -7,39 +7,53 @@ var fire_torch;
 var unicorn;
 var randomX;
 var y;
+var r;
 
 /*preload our images*/
 function preload() {
   blue_club = loadImage('/assets/blue_club.png');
+  pink_club = loadImage('/assets/pink_juggle_club.png');
+  red_ball = loadImage('/assets/red_ball.png');
+  purple_ball = loadImage('/assets/purple_ball.png');
+
 }
 
 function setup() {
+  /*set up a black canvas to cover the full display height and width*/
   createCanvas(displayWidth, displayHeight);
   background(0);
   /*Every few seconds we want a new object to appear at the top of the screen*/
   setInterval(objectAppear, 3000);
 }
 
-/*generate objects*/
+/*generate random objects*/
 function objectAppear(){
-  randomX = random(1, width);
-  
-  image(blue_club, randomX, 20, 15, 50);
- 
-  switch( 3 ){
+  randomX = random(1, width-20);
+  /*select a random number from 0 to 4*/
+  r = Math.floor(Math.random() * 4);
+  /* set y-axis */
+  y = 15;
+
+   switch( r ){
+    case 0:
+      console.log('purple');
+      image(purple_ball, randomX, y, 25, 25);
+      break;
     case 1:
-      image(purple_ball, 15, 20, 20, 20);
+      console.log('blue');
+      image(blue_club, randomX, y, 15, 50);
       break;
     case 2:
-      image(blue_club, 10, 20, 15, 50);
+      console.log('red');
+      image(red_ball, randomX, y, 25, 25);
       break;
     case 3:
-      image(red_ball, 10, 20, 20, 20);
-      break;
+      image(pink_club, randomX, y, 15, 50);
   }
 }
 
 function draw() {
+    y = y + 5;
   
 }
 
